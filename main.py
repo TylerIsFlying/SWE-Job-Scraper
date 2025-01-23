f941bc4d784036739abc4c61327feacf0ae72728
Tyler Peterson <54076402+TylerIsFlying@users.noreply.github.com>
2025-01-23 08:26:58 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+url = 'https://github.com/SimplifyJobs/Summer2025-Internships'
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, 'lxml')
+table_tags = soup.find_all('table')
+tr_tags = table_tags[1].find_all_next('tr')
+for tr_tag in tr_tags:
+    td_tags = tr_tag.find_all_next('td')
+    print(td_tags)
