c9f29673a903eea8fa66dfdf8e052aaa28e9a10f
Tyler <7314tylerpeterson@gmail.com>
2025-01-26 16:52:06 -0800

    Made excel file a little more clean.

--- SWESpreadsheet/swespreadsheet.py
+++ SWESpreadsheet/swespreadsheet.py
@@ -14,11 +14,11 @@ def load_excel(json_file_name: str, excel_file_name: str,datas):
     for col_num, category in enumerate(categories, start=1):
         workbook_active.cell(row=1, column=col_num, value=category)
     for index, data in enumerate(datas):
-        workbook_active.cell(row=index+2, column=1, value=' '.join(datas[index]['company']))
+        workbook_active.cell(row=index+2, column=1, value=' '.join(datas[index]['company']).title())
         workbook_active.cell(row=index+2, column=2, value=' '.join(datas[index]['url']))
-        workbook_active.cell(row=index+2, column=3, value=' '.join(datas[index]['role']))
-        workbook_active.cell(row=index+2, column=4, value=' '.join(datas[index]['location']))
-        workbook_active.cell(row=index+2, column=5, value=' '.join(datas[index]['date posted']))
+        workbook_active.cell(row=index+2, column=3, value=' '.join(datas[index]['role']).title())
+        workbook_active.cell(row=index+2, column=4, value=' '.join(datas[index]['location']).upper())
+        workbook_active.cell(row=index+2, column=5, value=' '.join(datas[index]['date posted']).title())
     workbook.save(f"{excel_file_name}.xlsx")
 if __name__ == "__main__":
     main()
